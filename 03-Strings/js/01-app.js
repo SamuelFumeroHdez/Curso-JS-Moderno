ea0ab38d8f32828a0935d05ec48d08838fc9838c
SamuelFumeroHdez <samuelfumerohdez@gmail.com>
2021-02-19 12:11:41 +0000

    ejercicio 2 de strings

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,3 @@
+const producto = "Monitor de 20 pulgadas";
+const producto2 = String('Monitor 24 pulgadas');
+const producto3 = new String('Monitor 27 pulgadas');
---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,7 @@
+const producto = 'Monitor 20';
+
+console.log(producto.length); //Longitud de la cadena
+
+console.log(producto.indexOf('Monitor')); //En que posicion está
+
+console.log(producto.includes('Tablet')); //Comprueba si está en la cadena
